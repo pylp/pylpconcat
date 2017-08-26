a3ddc45710baa3a18c7eea12320ce54ba722634a
Parks Projets <guigui.97@hotmail.fr>
2017-08-26 17:38:42 +0200

    Clean code for PyPI

--- pylpconcat/transformer.py
+++ pylpconcat/transformer.py
@@ -18,36 +18,36 @@ class concat(pylp.Transformer):
 		super().__init__()
 
 		self.filename = filename
-        self.sep = options.get('sep', '\n')
+		self.sep = options.get('sep', '\n')
 
-        self.first_file = None
-        self.buffer = ''
+		self.first_file = None
+		self.buffer = ''
 
 
 	# Function called when a file need to be transformed
 	async def transform(self, file):
 		# Ignore empty files
-        if not file.contents:
-            return
+		if not file.contents:
+			return
 
-        # Store the first file for cloning it
-        if not self.first_file:
-            self.first_file = file
-        
-        # Add the contents
-        self.buffer += file.contents + self.sep
-
-
-    # Function called when all files are transformed
-	async def flush(self):
-        # No files contatenated
+		# Store the first file for cloning it
 		if not self.first_file:
-            return
+			self.first_file = file
+		
+		# Add the contents
+		self.buffer += file.contents + self.sep
 
-        # Append the result file into the stream
-        self.append(pylp.File(
-            os.path.join(self.first_file.cwd, self.filename),
-            contents = self.buffer,
-            cwd = self.first_file.cwd,
-            base = self.first_file.base
-        ))
+
+	# Function called when all files are transformed
+	async def flush(self):
+		# No files contatenated
+		if not self.first_file:
+			return
+
+		# Append the result file into the stream
+		self.append(pylp.File(
+			os.path.join(self.first_file.cwd, self.filename),
+			contents = self.buffer,
+			cwd = self.first_file.cwd,
+			base = self.first_file.base
+		))
